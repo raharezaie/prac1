acef532b36cc5ada1ac38cdc2deb18affb0fc8f2
raharezaie <raha.rezaiefar@torontomu.ca>
2025-01-31 17:57:37 -0500

    Edited index.html in branch1

--- slack_bot/bot.py
+++ slack_bot/bot.py
@@ -7,8 +7,7 @@ slack_token = os.getenv("SLACK_BOT_TOKEN")
 client = WebClient(token=slack_token)
 
 try:
-    response = client.chat_postMessage(channel="#general", text="Hello 
-from my bot!")
+    response = client.chat_postMessage(channel="#github-repo", text="Hello from my bot!")
     print(response)
 except SlackApiError as e:
     print(f"Error: {e.response['error']}")
